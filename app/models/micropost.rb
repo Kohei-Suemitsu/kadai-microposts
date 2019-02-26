7a24f4429d55b1c305355cc5092e68ef2ebfcdc0
falcon <tech.sk0046@gmail.com>
2019-02-26 01:58:49 +0000

    add destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,6 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length: {maximum: 225 }
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :lovers, through: :favorites, source: :user
 end
